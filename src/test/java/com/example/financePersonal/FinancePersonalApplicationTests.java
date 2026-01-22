8cc3161a4ce5d332efc4d94b77b210eaacf93c5c
Gabriel Viana Santos <gabriel.viana.gv02@gmail.com>
2026-01-22 09:41:18 -0300

    Initial commit

---
+++ src/test/java/com/example/financePersonal/FinancePersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financePersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancePersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
